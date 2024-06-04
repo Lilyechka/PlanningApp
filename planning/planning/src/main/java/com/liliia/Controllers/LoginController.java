68abf1aa72817dad5c41bdc3aef0d7fff13d7184
Lilyechka <161700525+Lilyechka@users.noreply.github.com>
2024-06-04 15:43:46 +0300

    Add files via upload

--- planning/planning/src/main/java/com/liliia/Controllers/LoginController.java
+++ planning/planning/src/main/java/com/liliia/Controllers/LoginController.java
@@ -1,24 +1,14 @@
 package com.liliia.Controllers;
 
-import com.liliia.model.User;
-import com.liliia.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/login")
+@Controller
 public class LoginController {
 
-/*        @Autowired
-        private UserService userService;
-
-        @GetMapping
-        public List<User> getAllUsers() {
-            return userService.getAllUsers();
-        }*/
-
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "/tasks";
+    }
 }
--- planning/planning/src/main/java/com/liliia/Security/SecurityConfig.java
+++ planning/planning/src/main/java/com/liliia/Security/SecurityConfig.java
@@ -1,21 +1,15 @@
 package com.liliia.Security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
-import java.security.SecureRandom;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -27,30 +21,26 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-
-    private PasswordEncoder passwordEncoder() {
-        return null;
-    }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
+                .authorizeRequests((authz) -> authz
                         .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults());
         return http.build();
     }
+
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("Kira")
-                .password("12345678")
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user = User.withUsername("Kira")
+                .password(encoder.encode("12345678"))
                 .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(user);
     }
+
 
 }
 
