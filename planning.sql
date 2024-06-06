254bbba9950ff8c877358478ca27ba121c7b1031
Lilyechka <161700525+Lilyechka@users.noreply.github.com>
2024-06-06 14:38:20 +0300

    Add files via upload

---
+++ planning.sql
@@ -0,0 +1,38 @@
+USE planner;
+CREATE TABLE users (
+    id_user INTEGER AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    role VARCHAR(5) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    date_of_registration DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tasks (
+    id_task INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id_user INTEGER,
+    topic VARCHAR(500) NOT NULL,
+    date_of_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
+    deadline DATETIME,
+    description_task VARCHAR(10000),
+    FOREIGN KEY (id_user) REFERENCES users (id_user) ON DELETE CASCADE
+);
+
+INSERT INTO users (username, role, password, email)
+ VALUES 
+('Julia', 'ADMIN', '$2a$10$7QZjEvv5YV58z1jGxKZTZO96QGlFQUnIo1F8nJhZSv8EbU1rD8AXG', 'admin@example.com'), -- password: admin
+('Kira', 'USER', '$2a$10$7QZjEvv5YV58z1jGxKZTZO96QGlFQUnIo1F8nJhZSv8EbU1rD8AXG', 'user1@example.com'), -- password: user
+('Mira', 'USER', '$2a$10$7QZjEvv5YV58z1jGxKZTZO96QGlFQUnIo1F8nJhZSv8EbU1rD8AXG', 'user2@example.com'); -- password: user
+
+
+INSERT INTO tasks (id_user, topic, deadline, description_task)
+ VALUES 
+(2, 'Task for user1', '2024-06-15T12:00:00', 'Description for task1'),
+(3, 'Task for user2', '2024-06-20T12:00:00', 'Description for task2');
+
+
+-- DROP TABLE users;
+-- DROP TABLE tasks;
+
+
+
